14f89cf31029976d7c2214995c46df1a289173ad
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2024-08-12 15:09:51 +0900

    fixed all bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
-import SearchHistoryPage from "./pages/SearchHistorypage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
 import NotFoundPage from "./pages/404";
 import Footer from "./components/Foter";
 import { Toaster } from "react-hot-toast";
