d25447b7f36797d3f39bf89df56eb9a88458aeb0
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2025-08-28 22:30:27 +0900

    modified authuser

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { create } from "zustand";
 
+const API_URL = import.meta.env.VITE_API_URL;
 export const useAuthStore = create((set) => ({
 	user: null,
 	isSigningUp: false,
@@ -12,7 +13,7 @@ export const useAuthStore = create((set) => ({
 	signup: async (credentials) => {
 		set({ isSigningUp: true });
 		try {
-			const response = await axios.post("/api/v1/auth/signup", credentials);
+			const response = await axios.post(`${API_URL}/api/v1/auth/signup`,credentials);
 			set({ user: response.data.user, isSigningUp: false });
 			toast.success("Account created successfully");
 		} catch (error) {
@@ -23,7 +24,7 @@ export const useAuthStore = create((set) => ({
 	login: async (credentials) => {
 		set({ isLoggingIn: true });
 		try {
-			const response = await axios.post("/api/v1/auth/login", credentials);
+			const response = await axios.post(`${API_URL}/api/v1/auth/login`, credentials);
 			set({ user: response.data.user, isLoggingIn: false });
 		} catch (error) {
 			set({ isLoggingIn: false, user: null });
@@ -33,7 +34,7 @@ export const useAuthStore = create((set) => ({
 	logout: async () => {
 		set({ isLoggingOut: true });
 		try {
-			await axios.post("/api/v1/auth/logout");
+			await axios.post(`${API_URL}/api/v1/auth/logout`);
 			set({ user: null, isLoggingOut: false });
 			toast.success("Logged out successfully");
 		} catch (error) {
@@ -44,7 +45,7 @@ export const useAuthStore = create((set) => ({
 	authCheck: async () => {
 		set({ isCheckingAuth: true });
 		try {
-			const response = await axios.get("/api/v1/auth/authCheck");
+			const response = await axios.get(`${API_URL}/api/v1/auth/authCheck`);
 
 			set({ user: response.data.user, isCheckingAuth: false });
 		} catch (error) {
